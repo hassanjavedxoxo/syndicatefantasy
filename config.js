b63b0d3975b901e815ccfb3f13dea08b8d248289
MUHAMMAD HASSAN JAVED <mhassanjavedddd@gmail.com>
2024-11-30 10:03:49 +0500

    CHANGING MONGODB URI

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require("mongoose");
 
-const connnectDB = mongoose.connect(`mongodb+srv://hassanjavedwork:${process.env.MONGODB_USER_KEY}@cluster0.m5usp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+const connnectDB = mongoose.connect(`mongodb+srv://areesshah430:${process.env.MONGODB_USER_KEY}@cluster0.2hmiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
 module.exports = connnectDB;
